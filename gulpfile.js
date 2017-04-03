32f8d9098b4ec7f1217bc65956e431c7fc65e95e
Tim Urista <timothy.urista@gmail.com>
2017-04-02 21:13:32 -0700

    added gulp clean and rename tasks for css minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var postcss = require('gulp-postcss');
-var uglifycss = require('gulp-uglifycss');
+// var uglifycss = require('gulp-uglifycss');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 var gulp = require('gulp');
 
 gulp.task('css', function () {
     return gulp
         .src('./src/*.css')
         .pipe(postcss())
-        .pipe(uglifycss({'maxLineLen': 80, 'uglyComments': true}))
         .pipe(gulp.dest('./dist'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename('flexboxgrid60.min.css'))
+        .pipe(gulp.dest('./dist'));
 });
